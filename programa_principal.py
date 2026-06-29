a661f5b5dc604ee23cd700c3e081c8e5a3a6e792
Santii58 <Santii58.sn@gmail.com>
2026-06-29 05:49:25 -0300

    creo las opciones del menu principal con bloque try

--- programa_principal.py
+++ programa_principal.py
@@ -1,2 +1,36 @@
 #PROGRAMA PRINCIPAL
 import modulos
+
+while True:
+    try:
+        print("!!!MENU PRINCIPAL¡¡¡")
+        print("Opcion 1:Agregar pais")
+        print("Opcion 2:Actualizar datos(Poblacion y superficie)")
+        print("Opcion 3:Buscar un pais")
+        print("Opcion 4:Filtrar paises")
+        print("Opcion 5:Ordenar paises")
+        print("Opcion 6:Mostrar estadisticas")
+        print("Opcion 7:Salir")
+        opcion = int(input("Seleccione una opcion:"))
+        if opcion == 1:
+            modulos.agregar_pais()
+        elif opcion == 2:
+            modulos
+        elif opcion == 3:
+            modulos
+        elif opcion == 4:
+            modulos
+        elif opcion == 5:
+            modulos
+        elif opcion == 6:
+            modulos
+        elif opcion == 7:
+            break
+        else:
+            raise ValueError ("Ingrese una opcion valida")
+    except ValueError as e:
+        print(f"{e}")
+    except Exception:
+        print("ERRO INESPERADO")
+                   
+     
